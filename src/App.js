3264eec55f9ee2747bf07f0f20a41d52dec18d7c
jirenguliuk <707529659@qq.com>
2017-08-16 23:04:15 +0800

    install leancloud

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import * as localStore from './localStore'
+import AV from 'leancloud-storage'
+ var APP_ID = 'PV1tmOqS9J1H5BmkFzKvg6ML-9Nh9j0Va';
+ var APP_KEY = 'WUtpgrGPsk1I4sfqWKO70rbK';
+ AV.init({
+   appId: APP_ID,
+   appKey: APP_KEY
+ })
+ var TestObject = AV.Object.extend('TestObject')
+ var testObject = new TestObject()
+ testObject.save({
+   words: 'Hello World!'
+ }).then(function(object) {
+   alert('LeanCloud Rocks!')
+ })
+ 
  class App extends Component {
      constructor(props){
          super(props)
