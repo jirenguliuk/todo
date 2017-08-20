c025a47654c306c7f4f9e0f85fa222250514f84e
jirenguliuk <707529659@qq.com>
2017-08-21 00:34:47 +0800

    project

--- src/App.js
+++ src/App.js
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 import 'normalize.css';
 import './reset.css';
 import './App.css';
+
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
-import * as localStore from './localStore';
-import AV from 'leancloud-storage'
- var APP_ID = 'PV1tmOqS9J1H5BmkFzKvg6ML-9Nh9j0Va';
- var APP_KEY = 'WUtpgrGPsk1I4sfqWKO70rbK';
- AV.init({
-   appId: APP_ID,
-   appKey: APP_KEY
- })
- var TestObject = AV.Object.extend('TestObject')
- var testObject = new TestObject()
- testObject.save({
-   words: 'Hello World!'
- }).then(function(object) {
-   alert('LeanCloud Rocks!')
- })
- 
+import UserDialog from './UserDialog';
+import {getCurrentUser, signOut, TodoModel} from './leanCloud';
+//import * as localStore from './localStore';
+
+
  class App extends Component {
      constructor(props){
          super(props)
          this.state = {
+            user: getCurrentUser() || {},
             newTodo: '',
-            todoList: localStore.load('todoList') || []
+            todoList: []
          }
+             let user = getCurrentUser()
+             if (user) {
+               TodoModel.getByUser(user, (todos) => {
+                 let stateCopy = JSON.parse(JSON.stringify(this.state))
+                 stateCopy.todoList = todos
+                 this.setState(stateCopy)
+               })
+             }
      }
         render(){
             let todos = this.state.todoList
@@ -43,26 +42,50 @@ import AV from 'leancloud-storage'
 
             return(
                 <div className="App">
-                    <h1>我的待办</h1>
+                <h1>{this.state.user.username||'我'}的待办
+                  {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
+         </h1>
                     <div className="inputWrapper">
                        <TodoInput content={this.state.newTodo} 
                        onChange={this.changeTitle.bind(this)}
                        onSubmit={this.addTodo.bind(this)} />
                     </div>
-                    <ol>
+                    <ol className="todoList">
                     {todos}
                     
                     </ol>
+        {this.state.user.id ? 
+           null : 
+           <UserDialog 
+             onSignUp={this.onSignUpOrSignIn.bind(this)} 
+             onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
                 </div>
                 )
             }
+    signOut(){
+        signOut()
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.user = {}
+        this.setState(stateCopy)
+   }
+    onSignUpOrSignIn(user){
+        let stateCopy = JSON.parse(JSON.stringify(this.state)) 
+        stateCopy.user = user
+        this.setState(stateCopy)
+   }
             // componentDidUpdate 会在组件更新之后调用。如果我们默认「组件更新」等价于「数据更新」，那么就可以把 localStore.save('todoList', this.state.todoList) 写在这个钩子里。
       componentDidUpdate(){
-            localStore.save('todoList', this.state.todoList)
+            //localStore.save('todoList', this.state.todoList)
    }
         toggle(e, todo){
+            let oldStatus = todo.status
            todo.status = todo.status === 'completed' ? '' : 'completed'
-           this.setState(this.state)
+           TodoModel.update(todo, () => {
+          this.setState(this.state)
+        }, (error) => {
+          todo.status = oldStatus
+          this.setState(this.state)
+        })
            //localStore.save('todoList', this.state.todoList) 
    }    
         changeTitle(event){
@@ -73,28 +96,29 @@ import AV from 'leancloud-storage'
                // localStore.save('todoList', this.state.todoList)
         }    
         addTodo(event){
-            this.state.todoList.push({
-                id: idMaker(),
+            let newTodo = {
                 title: event.target.value,
                 status: null,
                 deleted: false
-           })
-            this.setState({
-                 newTodo: '',
-                 todoList: this.state.todoList
+           }
+           TodoModel.create(newTodo, (id) => {
+             newTodo.id = id
+                  this.state.todoList.push(newTodo)
+                  this.setState({
+                    newTodo: '',
+                    todoList: this.state.todoList
+                  })
+                }, (error) => {
+                  console.log(error)
         })
                // localStore.save('todoList', this.state.todoList)
     }
         delete(event, todo){
+            TodoModel.destroy(todo.id, () => {
                 todo.deleted = true
                 this.setState(this.state)
-               // localStore.save('todoList', this.state.todoList) 
+             })
+
   }
  }
 export default App;
-   let id = 0
- 
-   function idMaker(){
-    id += 1
-    return id
- }
